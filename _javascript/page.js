e4b0460b5b6d52c2cc805dbb7afef76e4f56b799
Steve Hoang <82992510+lotusk08@users.noreply.github.com>
2025-03-09 10:50:44 +0700

    feat(page): toc enable (not by default)

--- _javascript/page.js
+++ _javascript/page.js
@@ -1,12 +1,14 @@
 import { basic, initTopbar } from './modules/layouts';
 import {
   loadImg,
+  initToc,
   imgPopup,
   initClipboard,
   loadMermaid
 } from './modules/components';
 
 loadImg();
+initToc();
 imgPopup();
 initTopbar();
 initClipboard();
